0e7c804778c377ccc234b9b4c3d6657348941060
ehmohajer <eh.mohajer@gmail.com>
2020-03-24 19:52:39 +0430

    5-1-add Modal

---
+++ MadPay24.Data/DatabaseContext/MadpayDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MadPay24.Data.DatabaseContext
+{
+    class MadpayDbContext:DbContext
+    {
+        //Connection String
+         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Madpay24Db;integrated Security=True;MultipleActiveResultSets=True;");
+        }
+    }
+}
